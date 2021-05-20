a2d0b514efce92f59abea51f23e56fccffd13a67
Ashok Seervi <ashokseervi.ug@gmail.com>
2021-05-20 14:44:46 +0530

    Updated TennishCoach

--- spring-demo-annotation/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotation/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,24 +1,40 @@
 package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
-	
-	private FortuneService theFortuneService;
-	
+
 	@Autowired
-	public TennisCoach(FortuneService theFortuneService) {
-		this.theFortuneService = theFortuneService;
+	@Qualifier("randomFortuneService")
+	private FortuneService theFortuneService;
+
+//	@Autowired
+//	public TennisCoach(FortuneService theFortuneService) {
+//		this.theFortuneService = theFortuneService;
+//	}
+
+	public TennisCoach() {
+		System.out.println("TennisCoach:inside default constructor");
 	}
+
+	// define setter method
+//	@Autowired
+//	 public void setFortuneService(FortuneService theFortuneService) {
+//		System.out.println("TennisCoach : Setter Injection");
+//		 this.theFortuneService = theFortuneService;
+//	 }
 
 	@Override
 	public String getDailyWorkout() {
-		
-		return "Practice as Tennis Coach INstructions" ;
+
+		return "Practice as Tennis Coach INstructions";
 	}
-    
+
 	public String getDailyFortune() {
 		return theFortuneService.getFortune();
 	}
