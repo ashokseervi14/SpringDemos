056d49499affd887d5e2a962c6f10aa591ac7d9e
Ashok Seervi <ashokseervi.ug@gmail.com>
2021-05-20 16:21:59 +0530

    Added Config Class and Its DI

---
+++ spring-demo-annotation/src/com/luv2code/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,30 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class JavaConfigDemoApp {
+
+	public static void main(String[] args) {
+	
+		//read spring  java config file and loading
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(SportConfig.class);
+		
+		//retrive the bean
+		Coach theCoach = context.getBean("tennisCoach",Coach.class);
+		
+		
+		//use the bean
+		
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+		
+		//close the context
+		
+		context.close();
+		
+
+	}
+
+}
